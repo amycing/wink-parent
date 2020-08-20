6b47154a6bd892f21d79622f954812da587cf7e7
chengying <382816614@qq.com>
2020-08-20 21:54:30 +0800

    项目初始化

--- wink-summer/src/main/java/com/ying/summer/StartupApplication.java
+++ wink-summer/src/main/java/com/ying/summer/StartupApplication.java
@@ -2,16 +2,18 @@ package com.ying.summer;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by chengying
  */
 @SpringBootApplication
-@MapperScan("com.ying")
+@MapperScan(basePackages = "com.ying.summer.mapper")
 @EnableTransactionManagement(proxyTargetClass = true)
-public class StartupApplication  {
+public class StartupApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartupApplication.class, args);
         System.out.println("启动成功在端口:8989");
--- wink-summer/src/test/java/DemoApplicationTests.java
+++ wink-summer/src/test/java/DemoApplicationTests.java
@@ -1,20 +1,25 @@
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ying.summer.StartupApplication;
+import com.ying.summer.entity.WinkUser;
 import com.ying.summer.mapper.WinkUserMapper;
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-@ActiveProfiles("summer")
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StartupApplication.class)
-public class JunitTest {
+public class DemoApplicationTests {
+
+
+
     @Autowired
     private WinkUserMapper winkUserMapper;
-
     @Test
-    public void test() {
-        System.out.println(winkUserMapper.selectById("1"));
-    }
+    public void getBookById()
+        {
+            System.out.println(winkUserMapper.selectById("1"));
+        }
 }
